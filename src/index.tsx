c4d39482b961f15ba16f5c403b746d1e43ac1e71
lejinseop <js.lee@birdview.kr>
2021-07-05 10:37:24 +0900

    asd

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <App />,
+  </StrictMode>,
   document.getElementById('root'),
 );
 
