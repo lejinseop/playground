56615a386ad96c2f35e7c371b42ed338d70928bc
lejinseop <js.lee@birdview.kr>
2021-07-05 10:27:09 +0900

    feat: test 1

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,22 @@
 const CracoAlias = require('craco-alias');
 
 module.exports = {
+  babel: {
+    // https://emotion.sh/docs/css-prop
+    presets: [
+      // '@emotion/babel-preset-css-prop',
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic',
+          importSource: '@emotion/react',
+          // development: false,
+        },
+      ],
+    ],
+    // plugins: ['@emotion'],
+    plugins: ['@emotion/babel-plugin'],
+  },
   plugins: [
     {
       plugin: CracoAlias,
